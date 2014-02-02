3b247e7fa56c57f677feb473a7a98cdbd7ce5fe2
Bob Ippolito <bob@redivi.com>
2014-02-02 00:03:31 -0800

    timing

--- cron.sh
+++ cron.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+echo "[laptop-management] $(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+t0=$(date '+%s')
 git fetch origin
 git reset --hard origin/master
 cd $(dirname ${BASH_SOURCE[0]})
 ./run
+t1=$(date '+%s')
+dt=$((t1 - t0))
+echo "Finished in ${dt} seconds"
